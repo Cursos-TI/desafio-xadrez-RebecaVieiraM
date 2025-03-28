8b2908cdb878c94d6a9541da0c03ba868e7d716f
Rebeca Vieira Maia <147777974+RebecaVieiraM@users.noreply.github.com>
2025-03-28 13:45:18 +0000

    Correção da lógica da movimentação do bispo e do cavalo

--- xadrez.c
+++ xadrez.c
@@ -10,10 +10,12 @@
 
 // Implementação de Movimentação do Bispo
 void moverBispo(int passos) {
-    if (passos == 0) return; // Sai da recursão quando a peça já tiver percorrido todas as casas da jogada
-    printf("Cima, Direita\n"); // Exibe a movimentação
-    moverBispo(passos - 1); // Decrementa a variável para que se torne 0 e saia da recursão após percorrer as casas
-} 
+    for (int i = 0; i < passos; i++) { // Controla a quantidade de passos
+        for (int j = 0; j < 1; j++) { // Estrutura aninhada
+            printf("Cima, Direita\n");
+        }
+    }
+}
 
 // Implementação de Movimentação da Torre
 void moverTorre(int passos) {
@@ -30,32 +32,18 @@ void moverRainha(int passos) {
 }
 
 // Implementação de Movimentação do Cavalo (L)
-void moverCavalo(int passos) {
-    // Variáveis para controlar as direções 
-    char *direcaoHorizontal[] = {"Direita"}; // Movimento horizontal
-    char *direcaoVertical[] = {"Cima"};   // Movimento vertical
-
-    // Loop para controlar o número de movimentos do cavalo
-    for (int i = 0; i < passos; i++) {
-        int j = 0; // Variável para controlar os movimentos em L
-        while (j < 2) { // 2 movimentos para cada ciclo
-            // Movimento horizontal
-            printf("Movendo à %s\n", direcaoVertical[0]);
-            j++;
-            
-            // Verifica se devemos continuar e pular a parte do movimento vertical
-            if (j == 1) {
-                continue; // Pula a parte do movimento vertical se já fez o movimento horizontal
+void moverCavalo(int ciclos) {
+    for (int i = 0; i < ciclos; i++) {
+        int movimentos = 0;
+        while (movimentos <= 2) {
+            if (movimentos != 2) {
+                printf("Cima\n");
+                movimentos++;
+                continue; // Pula para a próxima iteração sem executar o código abaixo
             }
-
-            // Movimento vertical
-            printf("Movendo para %s\n", direcaoHorizontal[0]);
-            j++;
-
-            // Usando o break para terminar o loop quando o movimento for completo
-            if (j == 2) {
-                break;
-            }
+            printf("Direita\n");
+            movimentos++;
+            break; // Termina o loop ao completar o movimento em L
         }
     }
 }
